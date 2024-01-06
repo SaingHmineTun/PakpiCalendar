d60c19721fb10625c650d93f6c77e564b4305acb
Mein <saisainghminetun@gmail.com>
2024-01-06 17:07:35 +0630

    Improve PeeHtam and PeeMurng correctness. Added corresponding test files for them.

--- src/test/java/it/saimao/shancalendar/mmcalendar/ShanDateTest.java
+++ src/test/java/it/saimao/shancalendar/mmcalendar/ShanDateTest.java
@@ -7,16 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShanDateTest {
 
     @Test
-    void getPeeMurng() {
-        String peeMurng = ShanDate.getPeeMurng(2109);
-        assertEquals("ၵတ်းသႂ်ႉ(ငူး)", peeMurng);
+    void testPeeMurng() {
+        assertEquals("ၵတ်းသႂ်ႉ(ငူး)", ShanDate.getPeeMurng(2109));
+        assertEquals("ပိုၵ်းယီး(သိူဝ်)", ShanDate.getPeeMurng(2118));
     }
 
     @Test
-    void getPeeMurngKhe() {
-        assertEquals("ၼူ", ShanDate.getPeeHtam(1960));
-        assertEquals("ၼူ", ShanDate.getPeeHtam(1996));
-        assertEquals("ငူး", ShanDate.getPeeHtam(2001));
-        assertEquals("လိင်း", ShanDate.getPeeHtam(1992));
+    void testPeeHtam() {
+        assertEquals("ၶုတ်းၸႂ်ႉ(ၼူ)", ShanDate.getPeeHtam(1960));
+        assertEquals("ႁၢႆးၸႂ်ႉ(ၼူ)", ShanDate.getPeeHtam(1996));
+        assertEquals("ႁုင်ႉသႂ်ႉ(ငူး)", ShanDate.getPeeHtam(2001));
+        assertEquals("တဝ်ႇသၼ်(လိင်း)", ShanDate.getPeeHtam(1992));
+        assertEquals("ၵၢပ်ႇသီ(ငိူၵ်ႈ)", ShanDate.getPeeHtam(2024));
     }
 }
