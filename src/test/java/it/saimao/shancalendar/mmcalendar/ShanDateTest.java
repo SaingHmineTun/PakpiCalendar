a46aff05349a53cd5eec79acae45acd5a4ccd293
Mein <saisainghminetun@gmail.com>
2024-01-13 12:48:09 +0630

    Fixed test files!

--- src/test/java/it/saimao/shancalendar/mmcalendar/ShanDateTest.java
+++ src/test/java/it/saimao/shancalendar/mmcalendar/ShanDateTest.java
@@ -8,16 +8,16 @@ class ShanDateTest {
 
     @Test
     void testPeeMurng() {
-        assertEquals("ၵတ်းသႂ်ႉ(ငူး)", ShanDate.getPeeMurng(2109));
-        assertEquals("ပိုၵ်းယီး(သိူဝ်)", ShanDate.getPeeMurng(2118));
+        assertEquals("ၵတ်းသႂ်ႉ(မိင်ႈငူး)", ShanDate.getPeeMurng(2109));
+        assertEquals("ပိုၵ်းယီး(မိင်ႈသိူဝ်)", ShanDate.getPeeMurng(2118));
     }
 
     @Test
     void testPeeHtam() {
-        assertEquals("ၶုတ်းၸႂ်ႉ(ၼူ)", ShanDate.getPeeHtam(1960));
-        assertEquals("ႁၢႆးၸႂ်ႉ(ၼူ)", ShanDate.getPeeHtam(1996));
-        assertEquals("ႁုင်ႉသႂ်ႉ(ငူး)", ShanDate.getPeeHtam(2001));
-        assertEquals("တဝ်ႇသၼ်(လိင်း)", ShanDate.getPeeHtam(1992));
-        assertEquals("ၵၢပ်ႇသီ(ငိူၵ်ႈ)", ShanDate.getPeeHtam(2024));
+        assertEquals("ၶုတ်းၸႂ်ႉ(မိင်ႈၼူ)", ShanDate.getPeeHtam(1960));
+        assertEquals("ႁၢႆးၸႂ်ႉ(မိင်ႈၼူ)", ShanDate.getPeeHtam(1996));
+        assertEquals("ႁုင်ႉသႂ်ႉ(မိင်ႈငူး)", ShanDate.getPeeHtam(2001));
+        assertEquals("တဝ်ႇသၼ်(မိင်ႈလိင်း)", ShanDate.getPeeHtam(1992));
+        assertEquals("ၵၢပ်ႇသီ(မိင်ႈငိူၵ်ႈ)", ShanDate.getPeeHtam(2024));
     }
 }
